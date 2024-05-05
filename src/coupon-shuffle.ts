0db0130ab19b8d4fc279075cf5bf80770e16851d
Samuel Jensen <44519206+nichtsam@users.noreply.github.com>
2024-05-05 20:47:15 +0100

    test init

---
+++ src/coupon-shuffle.ts
@@ -0,0 +1,34 @@
+export function getAllPossibleCouponCombinations<
+  T extends { blockedTypes: Array<string> },
+>(coupons: Array<T>): Array<Array<T>> {
+  const alwaysCoupons: T[] = [];
+  const typeBuckets = coupons.reduce<Record<string, T[]>>((buckets, coupon) => {
+    if (coupon.blockedTypes.length === 0) {
+      alwaysCoupons.push(coupon);
+      return buckets;
+    }
+
+    for (const type of coupon.blockedTypes) {
+      const bucket = buckets[type];
+      if (!bucket) {
+        buckets[type] = [coupon];
+      } else {
+        bucket.push(coupon);
+      }
+    }
+
+    return buckets;
+  }, {});
+
+  const combinations: Array<Array<T>> = [];
+  const minimunCombo = alwaysCoupons ?? [];
+
+  let options = Object.keys(typeBuckets);
+
+  if (combinations.length === 0 && minimunCombo.length !== 0) {
+    combinations.push(minimunCombo);
+  }
+
+  // console.dir({ coupons, combinations, alwaysCoupons }, { depth: Infinity });
+  return combinations;
+}
