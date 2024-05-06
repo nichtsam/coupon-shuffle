51bf7ff37bb218fad249b1f3cfff3e1d74573800
Samuel Jensen <44519206+nichtsam@users.noreply.github.com>
2024-05-06 02:15:16 +0100

    brute force 😭

--- src/coupon-shuffle.ts
+++ src/coupon-shuffle.ts
@@ -1,94 +1,82 @@
 export function getAllPossibleCouponCombinations<
   T extends { blockedTypes: Array<string> },
 >(coupons: Array<T>): Array<Array<T>> {
-  (function debug() {
-    coupons = coupons.map((c, i) => {
-      // @ts-ignore
-      c.id = i;
-      return c;
-    });
-  })();
+  // (function debug() {
+  //   coupons = coupons.map((c, i) => {
+  //     // @ts-ignore
+  //     c.id = i;
+  //     return c;
+  //   });
+  // })();
 
-  const alwaysCoupons: T[] = [];
-  const typeBuckets = coupons.reduce<Record<string, T[]>>((buckets, coupon) => {
-    if (coupon.blockedTypes.length === 0) {
-      alwaysCoupons.push(coupon);
-      return buckets;
-    }
+  const possibleCombinations = generateIndexCombinations(coupons.length).map(
+    (arr) => arr.map((i) => coupons[i]!),
+  );
 
-    for (const type of coupon.blockedTypes) {
-      const bucket = buckets[type];
-      if (!bucket) {
-        buckets[type] = [coupon];
-      } else {
-        bucket.push(coupon);
-      }
-    }
+  const validCombinations = possibleCombinations
+    .filter(validateCombination)
+    .sort((a, b) => b.length - a.length);
 
-    return buckets;
-  }, {});
+  const uniq = removeSubsets(validCombinations);
 
-  const combinations: Array<Array<T>> = [];
-  const minimunCombo = alwaysCoupons ?? [];
-
-  const allBlockTypes = Object.keys(typeBuckets);
-
-  function helper(options: string[], currentCombo: T[]) {
-    // assuming options are all valid next choice
-
-    if (options.length === 0) {
-      // log({ options, currentCombo, combinations });
-      // throw "";
-      combinations.push(currentCombo);
-      return;
-    }
-
-    for (const choice of options) {
-      for (const coupon of typeBuckets[choice] ?? []) {
-        const blockTypes = currentCombo.reduce((set, c) => {
-          for (const t of c.blockedTypes) {
-            set.add(t);
-          }
-          return set;
-        }, new Set<string>());
-
-        // check if new coupon conflict with current combo
-        for (const t of coupon.blockedTypes) {
-          if (blockTypes.has(t)) {
-            combinations.push(currentCombo);
-            return;
-          }
-
-          // feel free to add, since blockTypes is local and it returns once a block is encountered
-          blockTypes.add(t);
-        }
-
-        const newCombo = [...currentCombo, coupon];
-
-        const newOptions = allBlockTypes.filter((t) => !blockTypes.has(t));
-
-        log({ currentCombo, coupon, newOptions, newCombo, blockTypes });
-        helper(newOptions, newCombo);
-      }
-    }
-  }
-
-  try {
-    helper(allBlockTypes, minimunCombo);
-  } catch {}
-
-  if (combinations.length === 0 && minimunCombo.length !== 0) {
-    combinations.push(minimunCombo);
-  }
-
-  log({
-    coupons,
-    // @ts-ignore
-    combinations: combinations.map((combo) => combo.map((c) => c.id).join(",")),
-  });
-  return combinations;
+  return uniq;
 }
 
-function log(item: any) {
-  console.dir(item, { depth: Infinity });
+function validateCombination<T extends { blockedTypes: Array<string> }>(
+  coupons: Array<T>,
+): boolean {
+  const blockTypes: Record<string, boolean> = {};
+  for (const coupon of coupons) {
+    for (const t of coupon.blockedTypes) {
+      if (blockTypes[t]) {
+        return false;
+      } else {
+        blockTypes[t] = true;
+      }
+    }
+  }
+  return true;
+}
+
+function generateIndexCombinations(length: number) {
+  const result: number[][] = [];
+
+  function helper(startIndex: number, currentCombination: number[]) {
+    if (currentCombination.length > 0) {
+      result.push(currentCombination.slice());
+    }
+
+    for (let i = startIndex; i < length; i++) {
+      currentCombination.push(i);
+      helper(i + 1, currentCombination);
+      currentCombination.pop();
+    }
+  }
+
+  helper(0, []);
+
+  return result;
+}
+
+function removeSubsets<T extends any>(arr: T[][]) {
+  let result = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    let isSubset = false;
+    for (let j = 0; j < arr.length; j++) {
+      if (i !== j && isSubsetOf(arr[i]!, arr[j]!)) {
+        isSubset = true;
+        break;
+      }
+    }
+    if (!isSubset) {
+      result.push(arr[i]!);
+    }
+  }
+
+  return result;
+}
+
+function isSubsetOf<T extends any>(subset: T[], superset: T[]) {
+  return subset.every((elem) => superset.includes(elem));
 }
